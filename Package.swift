dd27d740b5b3928ba672527126708ce5055d39f9
zubair.ismail <zubair.ismail@Zubair-Ismail.local>
2023-12-19 11:44:44 +0500

    SPM New Version

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
         
         .binaryTarget(
                 name: "ShuftiPro",
-                url:"https://github.com/shuftipro/shuftipro-framework/raw/main/ShuftiPro.xcframework.zip",
-                checksum: "5f6a39cef870c5138bef17040557ad6f7d7f6e0982505d29eaa566e8a12fcc68"
+                url:"https://github.com/shuftipro/ShuftiPro-Onsite-Framework/raw/main/ShuftiPro.xcframework.zip",
+                checksum: "d657b97b0c67bb7f18bcba2de592e72c6975d7f8b292726e7667ead07cb20c85"
                       ),
         .target(
             name: "PackageDependencies",
